62c49b53e739d7d9a888a08e15eb917c392abd58
tylerposegate <pos21001@byui.edu>
2022-03-30 09:00:35 -0600

    added to collisions

---
+++ mangame/scripting/handle_collisions.py
@@ -0,0 +1,38 @@
+class HandleCollisionsAction(action):
+    """
+    An update action that handles interactions between the actors.
+    
+    The responsibility of HandleCollisionsAction is to handle the situation when the snake collides
+    with the food, or the snake collides with its segments, or the game is over.
+
+    Attributes:
+        _is_game_over (boolean): Whether or not the game is over.
+    """
+
+    def __init__(self):
+        """Constructs a new HandleCollisionsAction."""
+        self._is_game_over = False
+
+    def execute(self, cast, script):
+        """Executes the handle collisions action.
+
+        Args:
+            cast (Cast): The cast of Actors in the game.
+            script (Script): The script of Actions in the game.
+        """
+        if not self._is_game_over:
+            """self._handle_food_collision(cast)"""
+            self._handle_trail_collision(cast)
+            self._handle_segment_collision(cast)
+            self._handle_game_over(cast)
+
+    def _handle_trail_collision(self, cast):
+        """Updates the score and moves the food if the man collides with the food.
+        
+        Args:
+            cast (Cast): The cast of Actors in the game.
+        """
+
+  
+      
+    
